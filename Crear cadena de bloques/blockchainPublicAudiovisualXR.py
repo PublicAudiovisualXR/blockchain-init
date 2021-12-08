2bdaa5178a356b0c758245771f6cd8e1932c3617
Jordi Martos <develop@publicaudiovisual.com>
2021-12-08 12:25:31 +0100

    MinarBloque

--- Crear cadena de bloques/blockchainPublicAudiovisualXR.py
+++ Crear cadena de bloques/blockchainPublicAudiovisualXR.py
@@ -74,10 +74,20 @@ app = Flask(__name__)
 # Crear una blockchain
 blockchain = Blockchain()
 
-
-
-
-
+# Minar un nuevo bloque
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message' : '¡Enhorabuena, has minando un nuevo bloque!',
+                'index' : block['index'],
+                'timestamp' : block['timestamp'],
+                'proof' : block['proof'],
+                'previous_hash' : block['previous_hash']}
+    return jsonify(response)
 
 
 
